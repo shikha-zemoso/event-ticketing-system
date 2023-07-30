b731e9b65dd7b09787c71ece8cdf8cc3929aca65
shikha-zemoso <shikha.yadav@zemosolabs.com>
2023-07-30 17:19:55 +0530

    add Global Exception Handler for Event ticketing system

---
+++ src/main/java/com/zemoso/eventticketingsystem/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.zemoso.eventticketingsystem.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundException(Exception ex) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+}
