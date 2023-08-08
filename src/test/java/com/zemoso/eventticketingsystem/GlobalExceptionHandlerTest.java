a04dc602e80e1d69b95fbf9e003a7ab6fd9713ee
shikha-zemoso <shikha.yadav@zemosolabs.com>
2023-08-08 21:18:44 +0530

    add test case for Event ticketing system

--- src/test/java/com/zemoso/eventticketingsystem/GlobalExceptionHandlerTest.java
+++ src/test/java/com/zemoso/eventticketingsystem/GlobalExceptionHandlerTest.java
@@ -1,14 +1,14 @@
 package com.zemoso.eventticketingsystem;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import com.zemoso.eventticketingsystem.exception.ErrorResponse;
-import com.zemoso.eventticketingsystem.exception.EventNotFoundException;
-import com.zemoso.eventticketingsystem.exception.GlobalExceptionHandler;
-import com.zemoso.eventticketingsystem.exception.VenueNotFoundException;
+
+import com.zemoso.eventticketingsystem.exception.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.io.IOException;
 
 public class GlobalExceptionHandlerTest {
 
@@ -47,6 +47,53 @@ public class GlobalExceptionHandlerTest {
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
         assertEquals(HttpStatus.NOT_FOUND.value(), response.getBody().getStatus());
         assertEquals("Venue not found", response.getBody().getMessage());
+    }
+
+    @Test
+    public void testHandleTicketNotFoundException() {
+        TicketNotFoundException exception = new TicketNotFoundException("Ticket not found");
+        ResponseEntity<ErrorResponse> response = exceptionHandler.handleNotFoundException(exception);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(HttpStatus.NOT_FOUND.value(), response.getBody().getStatus());
+        assertEquals("Ticket not found", response.getBody().getMessage());
+    }
+
+    @Test
+    public void testHandleNotEnoughSeatsException() {
+        NotEnoughSeatsException exception = new NotEnoughSeatsException("Not enough seats found");
+        ResponseEntity<ErrorResponse> response = exceptionHandler.handleNotFoundException(exception);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(HttpStatus.NOT_FOUND.value(), response.getBody().getStatus());
+        assertEquals("Not enough seats found", response.getBody().getMessage());
+    }
+
+    @Test
+    public void testHandleUserNotFoundException() {
+        UserNotFoundException exception = new UserNotFoundException("User not found");
+        ResponseEntity<ErrorResponse> response = exceptionHandler.handleNotFoundException(exception);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(HttpStatus.NOT_FOUND.value(), response.getBody().getStatus());
+        assertEquals("User not found", response.getBody().getMessage());
+    }
+
+
+    @Test
+    public void testHandleIOException() {
+        // Arrange
+        String errorMessage = "IO error";
+        IOException exception = new IOException(errorMessage);
+
+        // Act
+        ResponseEntity<ErrorResponse> responseEntity = exceptionHandler.handleNotFoundException(exception);
+        ErrorResponse errorResponse = responseEntity.getBody();
+
+        // Assert
+        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+        assertEquals(HttpStatus.NOT_FOUND.value(), errorResponse.getStatus());
+        assertEquals(errorMessage, errorResponse.getMessage());
     }
 
 }
